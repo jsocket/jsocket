263087dfc1d39b1dbcc4c5824bead05adbf6a8ee
Michal Krejci <michal@krejci.vip>
2018-12-05 17:16:55 +0100

    Update docs to reflect latest commit

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.jsocket"},{"l":"net.jsocket.client"},{"l":"net.jsocket.server"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.jsocket"},{"l":"net.jsocket.client"},{"l":"net.jsocket.server"},{"l":"net.jsocket.test"}]
